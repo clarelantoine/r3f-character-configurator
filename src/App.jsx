a2dd9d119033ebde700fc69955ef37baefd3f563
Clarel Antoine <clarel.antoine93@gmail.com>
2024-10-07 03:20:42 +0400

    :sparkles: setup zustand and pocketbase to fetch data from the backend

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import { OrbitControls } from "@react-three/drei";
 import { Canvas } from "@react-three/fiber";
+import { UI } from "./components/UI";
 
 export default function App() {
   return (
     <>
+      <UI />
       <Canvas camera={{ position: [3, 3, 3] }}>
         <color attach="background" args={["#333333"]} />
         <OrbitControls />
